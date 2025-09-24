0aabe44f2135f14a860ab049eb054b3a190e8b75
KATIA ALEXANDRA PALMA LOPEZ <202260688@ucc.mx>
2025-09-24 16:16:27 -0600

    Add files via upload

---
+++ ANDROID STUDIO/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.miprimeraapp;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        TextView pantalla;
+        Button boton;
+        EditText entrada;
+
+        pantalla = findViewById(R.id.screen);
+        boton = findViewById(R.id.click);
+        entrada = findViewById(R.id.enter);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String texto = entrada.getText().toString();
+                pantalla.setText(texto);
+                entrada.setText("");
+            }
+        });
+    }
+}
