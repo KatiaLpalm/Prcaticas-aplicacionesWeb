775d0f6d2e242275368081b05e62e659582b778f
KATIA ALEXANDRA PALMA LOPEZ <202260688@ucc.mx>
2025-09-24 16:28:02 -0600

    Add files via upload

--- REACT NATIVE/App.js
+++ REACT NATIVE/App.js
@@ -1,34 +1,22 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import IMCScreen from "./screens/IMCScreen";
+import DivisasScreen from "./screens/DivisasScreen";
+import PropinaScreen from "./screens/PropinaScreen";
 
-import React,{Children, useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+const Stack = createNativeStackNavigator();
 
-  const Texto =() => {
-    const[texto,setTexto]=useState('mas vale malo por conocido')
-    const actualizaTexto =()=>
-      setTexto ('que bueno por conocer')
-    return(
-      <Text onPress={actualizaTexto}>{texto}</Text>
-
-    )
-  }
 export default function App() {
   return (
-    <View style={styles.container}>
-       <text>Hola mundo reac native</text>
-       <Texto texto={'cambiado con props'}/>
-       <Texto texto={'esta es mi clase favorita'}/>
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="IMC" component={IMCScreen} />
+        <Stack.Screen name="Cambio de Divisas" component={DivisasScreen} />
+        <Stack.Screen name="Propina" component={PropinaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
